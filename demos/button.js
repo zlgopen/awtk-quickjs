9e1a4917fb7b5ac14c3fa9841dc3dd78b1c8fff6
lixianjing <xianjimli@hotmail.com>
2019-12-03 18:02:17 -0800

    sync

--- demos/button.js
+++ demos/button.js
@@ -3,12 +3,15 @@ function applicationInit() {
   var ok = TButton.create(win, 0, 0, 0, 0);
 
   ok.setText("ok");
+  ok.setName("ok");
   ok.setSelfLayoutParams("center", "middle", "50%", "30");
 
   ok.on(TEventType.CLICK, function(evt) {
     var e = TPointerEvent.cast(evt);
+    var target = TButton.cast(e.target);
+
+    console.log("click at " + e.x + " " + e.y + " on " + target.name);
     console.log("global: " + TGlobal.getPointerX() + " " + TGlobal.getPointerY() + " " + TGlobal.isPointerPressed());
-    console.log("on click: " + e.x + " " + e.y);
 
     return TRet.OK;
   });
