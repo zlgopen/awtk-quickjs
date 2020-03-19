6d38c83e4b19a21842885e4c412920c09d34eb96
xianjimli <xianjimli@gmail.com>
2020-03-19 08:08:34 +0800

    update assets

--- sync_assets.sh
+++ sync_assets.sh
@@ -2,6 +2,6 @@
 
 cp -rf ../awtk/demos/assets .
 cp -rf ../awtk/scripts/update_res_app.py scripts/update_res.py
-
+rm -rf src/assets*.*
 python scripts/update_res.py clean
 python scripts/update_res.py all
