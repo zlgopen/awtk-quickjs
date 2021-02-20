adbc24de6edf1ba2a3549657c2641eb9dcf3fa21
lixianjing <xianjimli@hotmail.com>
2021-02-20 16:33:47 +0800

    update build scripts

--- scripts/awtk_locator.py
+++ scripts/awtk_locator.py
@@ -1,50 +1,62 @@
 import os
 import sys
 
-
-def getAwtkConfig(LINUX_FB):
-    AWTK_ROOT = getAwtkOrAwtkLinuxFbRoot(LINUX_FB)
-    sys.path.insert(0, AWTK_ROOT)
-    import awtk_config as awtk
-
-    return awtk
-
-
-def getAwtkOrAwtkLinuxFbRoot(LINUX_FB):
-    if LINUX_FB:
-        AWTK_ROOT = getAwtkLinuxFbRoot()
-    else:
-        AWTK_ROOT = getAwtkRoot()
-
-    return AWTK_ROOT
-
-
-def getAwtkScriptsRoot():
-    return os.path.join(locateAWTK('awtk'), 'scripts')
-
+AWTK_ROOT = ''
 
 def getAwtkRoot():
-    return locateAWTK('awtk')
+    return AWTK_ROOT
 
+def getAwtkSDKPath():
+    env = os.environ
+    if 'AWTK_SDK_PATH' in env:
+        return env['AWTK_SDK_PATH']
+    else:
+        return ''
 
-def getAwtkLinuxFbRoot():
-    return locateAWTK('awtk-linux-fb')
-
+def getAwtkOrAwtkLinuxFbRoot(is_linux_fb):
+    if is_linux_fb:
+        return locateAWTK('awtk-linux-fb')
+    else:
+        return locateAWTK('awtk')
 
 def locateAWTK(awtk):
-    awtk_root = '../' + awtk
+    awtk_root = ''
+
     if not os.path.exists(awtk_root):
         dirnames = ['../'+awtk, '../../'+awtk, '../../../'+awtk]
         for dirname in dirnames:
             if os.path.exists(dirname):
                 awtk_root = dirname
                 break
+
+    if not os.path.exists(awtk_root):
+        awtk_sdk_path = getAwtkSDKPath();
+        if os.path.exists(awtk_sdk_path):
+            awtk_root = awtk_sdk_path + '/' + awtk
+
     return os.path.abspath(awtk_root)
 
+def init(ARGUMENTS = None):
+    global AWTK_ROOT
+    global LINUX_FB
 
-AWTK_ROOT = getAwtkRoot()
-AWTK_SCRIPTS_ROOT = getAwtkScriptsRoot()
-sys.path.insert(0, AWTK_SCRIPTS_ROOT)
+    if ARGUMENTS:
+        AWTK_ROOT = ARGUMENTS.get('AWTK_ROOT', '')
+        LINUX_FB = ARGUMENTS.get('LINUX_FB', '')
+    else:
+        LINUX_FB = ''
+    
+    if not os.path.exists(AWTK_ROOT):
+        AWTK_ROOT = getAwtkOrAwtkLinuxFbRoot(LINUX_FB != '')
+    elif os.path.exists(LINUX_FB):
+        print(' do not set LINUX_FB and AWTK_ROOT !!!')
+        sys.exit()
 
-print('AWTK_ROOT:' + AWTK_ROOT)
-print('AWTK_SCRIPTS_ROOT:' + AWTK_SCRIPTS_ROOT)
+    if LINUX_FB:
+        AWTK_SCRIPTS_ROOT = os.path.join(AWTK_ROOT, '../awtk/scripts')
+    else:
+        AWTK_SCRIPTS_ROOT = os.path.join(AWTK_ROOT, 'scripts')
+    sys.path.insert(0, AWTK_SCRIPTS_ROOT)
+
+    print('AWTK_ROOT: ' + AWTK_ROOT)
+    print('AWTK_SCRIPTS_ROOT: ' + AWTK_SCRIPTS_ROOT)
